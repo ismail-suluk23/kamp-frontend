9b058278b6bb91a11574e130ae9989e10301ba26
iso suluk <dede.suluk.1970@gmail.com>
2021-03-11 00:25:34 +0300

    11 mart ders sonu

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ProductResponseModel } from '../models/productResponseModel';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  apiUrl="https://localhost:44304/api/products/getall";
+  constructor(private httpCliend:HttpClient) { }
+  
+  getProducts():Observable<ProductResponseModel>{
+   return this.httpCliend.get<ProductResponseModel>(this.apiUrl);
+  }
+}
