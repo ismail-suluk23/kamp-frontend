7132ece40487107fbb408316a884d4eb2c7d640d
iso suluk <dede.suluk.1970@gmail.com>
2021-03-11 00:51:45 +0300

    11 mart

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -5,10 +5,10 @@ import { ProductResponseModel } from '../models/productResponseModel';
 
 @Injectable({  
   providedIn: 'root'
-})
+}) 
 export class ProductService {
   apiUrl="https://localhost:44304/api/products/getall";
-  constructor(private httpCliend:HttpClient) { }
+  constructor(private httpCliend:HttpClient) { } 
   
   getProducts():Observable<ProductResponseModel>{ 
    return this.httpCliend.get<ProductResponseModel>(this.apiUrl);
