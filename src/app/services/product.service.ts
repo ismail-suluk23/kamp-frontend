301c357331c0af3a5bffc9aef0976333a1c331df
iso suluk <dede.suluk.1970@gmail.com>
2021-03-11 00:31:22 +0300

    11 mart ders sonu

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -10,7 +10,7 @@ export class ProductService {
   apiUrl="https://localhost:44304/api/products/getall";
   constructor(private httpCliend:HttpClient) { }
   
-  getProducts():Observable<ProductResponseModel>{
+  getProducts():Observable<ProductResponseModel>{ 
    return this.httpCliend.get<ProductResponseModel>(this.apiUrl);
   }
 }
